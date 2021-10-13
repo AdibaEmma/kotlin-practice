b6207c76e207250d53f18d4d0a82c01754895081
Adiba Aweperi Emmanuel <eabaagah@gmail.com>
2021-10-13 10:54:31 +0000

    update

--- src/main/kotlin/arithmetic.kt
+++ src/main/kotlin/arithmetic.kt
@@ -17,4 +17,21 @@ fun main(args: Array<String>) {
 
     result = number1 % number2
     println("number1 % number2 = $result")
+
+
+
+    result = number1.plus(number2)
+    println("number1 + number2 = $result")
+
+    result = number1.minus(number2)
+    println("number1 - number2 = $result")
+
+    result = number1.times(number2)
+    println("number1 * number2 = $result")
+
+    result = number1.div(number2)
+    println("number1 / number2 = $result")
+
+    result = number1.mod(number2)
+    println("number1 % number2 = $result")
 }
