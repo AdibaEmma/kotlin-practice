375fedd871bd491ff92883cf933be9099325d2d8
Adiba Aweperi Emmanuel <eabaagah@gmail.com>
2021-10-13 14:26:26 +0000

    upate

--- src/main/kotlin/tlcLabs/kotlinSyntax/main.kt
+++ src/main/kotlin/tlcLabs/kotlinSyntax/main.kt
@@ -1,5 +1,29 @@
 package tlcLabs.kotlinSyntax
 
+var funds : Double = 100.0
+val pswd = "password"
+
 fun main(args: Array<String>) {
 
-}
+    fun balance() : Unit {
+        println(funds)
+    }
+
+    fun deposit(amount : Double) : Double {
+        return amount
+    }
+
+    var input : String
+    var cmd : List<String>
+
+    while (true) {
+        print("Command: ")
+        input = readLine()!!
+        cmd = input.split(" ")
+        when (cmd[0]) {
+            "balance" -> balance()
+            "deposit" -> println(deposit(cmd[1].toDouble()))
+            else -> println("Invalid command")
+        }
+    }
+    }
